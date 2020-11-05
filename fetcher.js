ea05a1cc4f978484f552a99dd17b66aa68702b68
Sean Oyler <xrysen@gmail.com>
2020-11-05 02:03:22 +0000

    Created a page fetcher in JS

---
+++ fetcher.js
@@ -0,0 +1,18 @@
+const request = require('request');
+const fs = require('fs');
+const args = process.argv.slice(2);
+const url = args[0].toString();
+const file = args[1].toString();
+
+request(url, (error, response, body) => {
+  if (!error)
+    console.log("Connected!");
+  fs.writeFile(file, body, (err) => {
+
+    const stats = fs.statSync(file);
+    if (err)
+      console.log(err);
+    console.log(`Downloaded and saved ${stats.size} bytes to ${args[1].toString()}`);
+  });
+});
+
